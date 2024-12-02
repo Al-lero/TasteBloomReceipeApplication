b6454bbd2dd1ec83a942797978845ec3a25ed16b
Al-lero <aleroutieyione@gmail.com>
2024-12-02 18:47:28 +0100

    first commit

---
+++ src/main/java/com/tastebloomprototype/tastebloomapplication/TasteBloomApplication.java
@@ -0,0 +1,13 @@
+package com.tastebloomprototype.tastebloomapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteBloomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasteBloomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tastebloomprototype/tastebloomapplication/TasteBloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tastebloomprototype.tastebloomapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteBloomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
